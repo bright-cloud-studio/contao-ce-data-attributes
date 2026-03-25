4c97d61998330049b1675f20d09299f2d2ec9602
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-25 13:47:13 -0400

    Create BcsDataAttributesBundle.php

---
+++ src/BcsDataAttributesBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Content Element Data Attributes
+ * @license    LGPL-3.0+
+ * @see        https://github.com/bright-cloud-studio/contao-ce-data-attributes
+ */
+
+namespace Bcs\DataAttributesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsDataAttributesBundle extends Bundle
+{
+}
