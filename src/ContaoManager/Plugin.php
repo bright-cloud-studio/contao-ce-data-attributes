16260971c8aa1284e4e1e8cbce852d9cc9ef0ef8
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-25 13:48:54 -0400

    Create Plugin.php

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Content Element Data Attributes
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-data-attributes
+ */
+
+namespace Bcs\DataAttributesBundle\ContaoManager;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create('Bcs\DataAttributesBundle\BcsDataAttributesBundle')
+                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle']),
+        ];
+    }
+}
