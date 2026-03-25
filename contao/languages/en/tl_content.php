0df75ce222212441e3e15a4d9aa28c593741826c
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-25 15:23:34 -0400

    Create tl_content.php

---
+++ contao/languages/en/tl_content.php
@@ -0,0 +1,8 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_content']['customDataAttributes'] = [
+    'Data attributes',
+    'Add custom data-* attributes to the content element wrapper.',
+];
+
+$GLOBALS['TL_LANG']['tl_content']['data_attributes_legend'] = 'Data attributes';
