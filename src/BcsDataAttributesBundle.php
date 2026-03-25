1bf370c60d4a94eae507742c3128d4ffb288fe02
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-25 14:58:31 -0400

    Update BcsDataAttributesBundle.php

--- src/BcsDataAttributesBundle.php
+++ src/BcsDataAttributesBundle.php
@@ -10,8 +10,14 @@
 
 namespace Bcs\DataAttributesBundle;
 
+use Bcs\DataAttributesBundle\DependencyInjection\BcsDataAttributesExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BcsDataAttributesBundle extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new BcsDataAttributesExtension();
+    }
 }
