80ae8e71c69486dabed8e9bf8b374f396ccaa84d
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-26 11:42:43 -0400

    Update tl_content.php

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -9,41 +9,42 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['ce_data_attributes'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_content']['ce_data_attributes'],
     'exclude' => true,
     'inputType' => 'rowWizard',
-    'eval' => [
-        'tl_class' => 'clr',
-        'columnFields' => [
-            'attribute_id' => [
-                'label' => &$GLOBALS['TL_LANG']['tl_content']['ce_data_key'],
-                'inputType' => 'select',
-                'options_callback' => static function () {
-                    $options = [];
 
-                    $result = Database::getInstance()
-                        ->execute("SELECT id, label, attribute_name FROM tl_data_attribute WHERE published='1' ORDER BY label ASC");
+    'fields' => [
+        'attribute_id' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['ce_data_key'],
+            'inputType' => 'select',
+            'options_callback' => static function () {
+                $options = [];
 
-                    while ($result->next()) {
-                        $options[$result->id] = sprintf('%s [data-%s]', $result->label, $result->attribute_name);
-                    }
+                $result = \Contao\Database::getInstance()
+                    ->execute("SELECT id, label, attribute_name FROM tl_data_attribute WHERE published='1' ORDER BY label ASC");
 
-                    return $options;
-                },
-                'eval' => [
-                    'includeBlankOption' => true,
-                    'chosen' => true,
-                    'style' => 'width:300px',
-                ],
+                while ($result->next()) {
+                    $options[$result->id] = sprintf('%s [data-%s]', $result->label, $result->attribute_name);
+                }
+
+                return $options;
+            },
+            'eval' => [
+                'includeBlankOption' => true,
+                'chosen' => true,
             ],
-            'value' => [
-                'label' => &$GLOBALS['TL_LANG']['tl_content']['ce_data_value'],
-                'inputType' => 'text',
-                'eval' => [
-                    'maxlength' => 255,
-                    'style' => 'width:300px',
-                    'decodeEntities' => true,
-                ],
+        ],
+        'value' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['ce_data_value'],
+            'inputType' => 'text',
+            'eval' => [
+                'maxlength' => 255,
+                'decodeEntities' => true,
             ],
         ],
     ],
+
+    'eval' => [
+        'tl_class' => 'clr',
+    ],
+
     'save_callback' => [
         [[\Bcs\DataAttributesBundle\Dca\ContentDataAttributesCallback::class, 'validateAndNormalize']],
     ],
