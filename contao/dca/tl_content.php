0cc70c6493a3fbf954ef2e815bdd3d9b5d76b12b
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-26 10:21:59 -0400

    Update tl_content.php

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -7,9 +7,21 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['ce_data_attributes'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['ce_data_attributes'],
     'inputType' => 'dataAttributeWizard',
     'eval'      => [
-        'tl_class'       => 'clr',
-        'decodeEntities' => true, // Add this to ensure proper data handling
+        'tl_class' => 'clr',
+        'columnFields' => [ // If this is a KeyValue style wizard, it needs columns
+            'key' => [
+                'label' => &$GLOBALS['TL_LANG']['tl_content']['ce_data_key'],
+                'inputType' => 'text',
+                'eval' => ['style'=>'width:40%', 'nospace'=>true]
+            ],
+            'value' => [
+                'label' => &$GLOBALS['TL_LANG']['tl_content']['ce_data_value'],
+                'inputType' => 'text',
+                'eval' => ['style'=>'width:50%']
+            ]
+        ]
     ],
+    // THIS IS THE KEY:
     'sql'       => "blob NULL", 
 ];
 
