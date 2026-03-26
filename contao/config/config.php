2e3b06679be017f57b91a6f347dafabb0be60900
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-26 11:34:46 -0400

    Update config.php

--- contao/config/config.php
+++ contao/config/config.php
@@ -5,8 +5,5 @@ $GLOBALS['BE_MOD']['content']['data_attributes'] = [
     'tables' => ['tl_data_attribute'],
 ];
 
-// Register the custom wizard widget
-$GLOBALS['BE_FFL']['dataAttributeWizard'] = \Bcs\DataAttributesBundle\Widget\DataAttributeWizard::class;
-
 // Register the frontend parseTemplate hook
 $GLOBALS['TL_HOOKS']['parseTemplate'][] = [\Bcs\DataAttributesBundle\EventListener\ParseTemplateListener::class, '__invoke'];
