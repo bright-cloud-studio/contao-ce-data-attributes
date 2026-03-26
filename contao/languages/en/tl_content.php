b27311a02a48eeeea84a07f7a5ad3c6b50af4b1c
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-26 09:48:15 -0400

    Create tl_content.php

---
+++ contao/languages/en/tl_content.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_content']['ce_data_attributes']    = ['Data Attributes', 'Select data attributes and optional values to apply to this content element.'];
+$GLOBALS['TL_LANG']['tl_content']['data_attributes_legend'] = 'Data Attributes';
