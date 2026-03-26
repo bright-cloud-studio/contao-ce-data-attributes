f1b5cd64d3ca39882d640937c575902e2d90efb9
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-26 09:47:57 -0400

    Create tl_content.php

---
+++ contao/dca/tl_content.php
@@ -0,0 +1,17 @@
+<?php
+
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+
+// Add the new field definition
+$GLOBALS['TL_DCA']['tl_content']['fields']['ce_data_attributes'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_content']['ce_data_attributes'],
+    'inputType' => 'dataAttributeWizard',
+    'eval'      => ['tl_class' => 'clr'],
+    'sql'       => 'blob NULL',
+];
+
+// Inject a new legend + field into the 'text' palette only
+PaletteManipulator::create()
+    ->addLegend('data_attributes_legend', 'expert_legend', PaletteManipulator::POSITION_BEFORE, true)
+    ->addField('ce_data_attributes', 'data_attributes_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('text', 'tl_content');
